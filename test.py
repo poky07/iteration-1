3ce66b08c5979b9da396a3f17839c7decf286f0b
Anika <poky071@gmail.com>
2017-10-25 18:34:06 -0600

    Update test.py
    Added average and score dropper

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 from iteration import *
 
-scores = [100, 90, 85, 100, 0]
+#scores = [100, 90, 85, 100, 0]
 
-iterate(scores)
+#print(scores)
+#scores = add_one(scores)
+#print(scores)
 
-names = ["Amy", "Bill", "Carol", "Dave", "Emily"]
+#names = ["Amy", "Bill", "Carol", "Dave", "Emily"]
 
-iterate(names)
+#print(names)
 
-print_scores(names, scores)
+#print_scores(names, scores)
 
-congratulations(names, scores)
+#congratulations(names, scores)
+
+scores = [1, 3, 4, 2]
+
+print sum(scores)
+
+#print max(scores)
+
+print average(scores)
+print drop(scores)
+#Write a function that finds the average of scores
+#Write a function that also finds the average but drops the lowest two scores
