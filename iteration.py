c1b165f31d091f83230d128a60d5760605bc3862
Anika <poky071@gmail.com>
2017-10-25 18:33:13 -0600

    Update iteration.py
    Added average and score dropper

--- iteration.py
+++ iteration.py
@@ -1,10 +1,10 @@
-# Make a local change
-#make achange from home
+
 # iteration pattern
+#doing the same thing once for each member of a list
 
 # [1, 5, 7 ,8 , 4, 3]
 
-def iterate(list):
+def print_list(list):
 	# standard for loop with range
 	# for i in range(0, len(list)):
 	# 	print list[i]
@@ -12,6 +12,11 @@ def iterate(list):
 	# for each loop
 	for item in list:
 		print item
+def add_one(list):
+	#standard for loop with range
+	for i in range(0, len(list)):
+		list[i] += 1
+	return list
 
 def print_scores(names, scores):
 	for i in range(0, len(names)):
@@ -19,7 +24,31 @@ def print_scores(names, scores):
 
 
 # filter pattern
+# exlude a calculation from list members.
 def congratulations(names, scores):
 	for i in range(0, len(names)):
 		if (scores[i] == 100):
 			print "Congrats", names[i], "! You got a perfect score!"
+
+
+
+# accumulation pattern - a type of iteration
+# keep track of other data as we go
+# def sum(list):
+# 	total = 0
+# 	for n in numbers:
+# 		total += n
+# 	return total
+
+# def max(numbers):
+# 	current_max = numbers[0]
+# 	for n in numbers:
+# 		if n > current_max:
+# 			current_max = n
+# 	return current_max
+def average(numbers):
+	return sum(numbers)/float(len(numbers))
+def drop(numbers):
+	for i in range(0, 2):
+		 numbers.remove(min(numbers))
+	return sum(numbers)/float(len(numbers))
